2be358e4591a82a8410ea57c2f8fd71b39b2e070
Diogo K <diogo.kamioka@gmail.com>
2015-11-11 17:27:15 -0200

    Fixing gifts migration

--- db/migrate/20151111054531_create_gifts.rb
+++ db/migrate/20151111054531_create_gifts.rb
@@ -2,7 +2,7 @@ class CreateGifts < ActiveRecord::Migration
   def change
     create_table :gifts do |t|
       t.string :name
-      t.bool :status
+      t.boolean :status
 
       t.timestamps null: false
     end
